01b3298e5d2c96ea8c9f3d102a937c42742b393c
XMZFPC <pengcheng.fan@outlook.com>
2020-08-11 19:32:32 +0200

    elegant solution, use recursive!!!

---
+++ LinkedList/medium/24.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head and head.next:
+            tmp = head.next
+            head.next = self.swapPairs(tmp.next)
+            tmp.next = head
+            return tmp
+        return head
